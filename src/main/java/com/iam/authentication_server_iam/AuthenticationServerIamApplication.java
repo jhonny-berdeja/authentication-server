99bdfa9c36bccab6b80acfdaadb4b90ae80b6f2c
jhonny-berdeja <jhonny.berdeja.100@gmail.com>
2024-09-08 22:46:13 -0300

    project created, added dependency and basic configuration

---
+++ src/main/java/com/iam/authentication_server_iam/AuthenticationServerIamApplication.java
@@ -0,0 +1,13 @@
+package com.iam.authentication_server_iam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerIamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServerIamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iam/authentication_server_iam/AuthenticationServerIamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iam.authentication_server_iam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServerIamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
